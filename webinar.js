69f51940e712af498b19efca8c2eeee23b7e7301
Stephen Hunnicutt <undecovecal@gmail.com>
2019-08-21 03:37:17 -0400

    Working on javascript

---
+++ webinar.js
@@ -0,0 +1,20 @@
+function sameBox(checkboxElem) {	
+	if (checkboxElem.name == "register1") {
+		document.getElementsByName("register2")[0].checked = checkboxElem.checked;
+	} else {
+		document.getElementsByName("register1")[0].checked = checkboxElem.checked;
+	}
+}
+
+function submitRegistration() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        	var responseObject = JSON.parse(this.responseText);
+			var responseBody = JSON.parse(responseObject.body);
+			alert(responseBody.submitok);
+       }
+    };
+	xhttp.open("GET", "https://bl45immth4.execute-api.us-east-1.amazonaws.com/production/", true);
+    xhttp.send(); 
+}
