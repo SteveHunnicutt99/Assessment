524c7e53162cef9b41f91badf4d903e43996c315
Stephen Hunnicutt <undecovecal@gmail.com>
2019-08-21 03:50:54 -0400

    Still more javascript

--- webinar.js
+++ webinar.js
@@ -1,21 +1,25 @@
 function sameBox(checkboxElem) {	
-	if (checkboxElem.name == "register1") {
-		document.getElementsByName("register2")[0].checked = checkboxElem.checked;
+	if (checkboxElem.id == "register1") {
+		document.getElementById("register2").checked = checkboxElem.checked;
 	} else {
-		document.getElementsByName("register1")[0].checked = checkboxElem.checked;
+		document.getElementById("register1").checked = checkboxElem.checked;
 	}
 }
 
 function submitRegistration() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-        	var responseObject = JSON.parse(this.responseText);
-			var responseBody = JSON.parse(responseObject.body);
-			document.getElementById("response1").innerText = responseBody.submitok;
-			document.getElementById("response2").innerText = responseBody.submitok;
-       }
-    };
-	xhttp.open("GET", "https://bl45immth4.execute-api.us-east-1.amazonaws.com/production/", true);
-    xhttp.send(); 
+	if (document.getElementById("register1").checked) {
+	    var xhttp = new XMLHttpRequest();
+	    xhttp.onreadystatechange = function() {
+	        if (this.readyState == 4 && this.status == 200) {
+	        	var responseObject = JSON.parse(this.responseText);
+				var responseBody = JSON.parse(responseObject.body);
+				document.getElementById("response1").innerText = responseBody.submitok;
+				document.getElementById("response2").innerText = responseBody.submitok;
+	       }
+	    };
+		xhttp.open("GET", "https://bl45immth4.execute-api.us-east-1.amazonaws.com/production/", true);
+	    xhttp.send();
+	} else {
+		alert("Please check the box for your free newsletter to register.");
+	}
 }
